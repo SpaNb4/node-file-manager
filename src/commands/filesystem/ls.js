1bf1579c375883999871ac495a9cefde84dc9aa6
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-02-04 18:06:39 +0300

    add sorting for ls

--- src/commands/filesystem/ls.js
+++ src/commands/filesystem/ls.js
@@ -6,14 +6,25 @@ export const ls = async () => {
   const files = await fs.readdir(cwd());
   const fileInfoArray = [];
 
-  // TODO add file sorting
+  const folders = [];
+  const filesList = [];
   for (const file of files) {
     const fileStat = await fs.lstat(path.join(cwd(), file));
     const isFile = fileStat.isFile();
     const fileType = isFile ? 'file' : 'directory';
+    const fileInfo = { Name: file, Type: fileType };
 
-    fileInfoArray.push({ Name: file, Type: fileType });
+    if (fileType === 'directory') {
+      folders.push(fileInfo);
+    } else {
+      filesList.push(fileInfo);
+    }
   }
+
+  folders.sort((a, b) => a.Name.localeCompare(b.Name));
+  filesList.sort((a, b) => a.Name.localeCompare(b.Name));
+
+  fileInfoArray.push(...folders, ...filesList);
 
   console.table(fileInfoArray, ['Name', 'Type']);
 };
