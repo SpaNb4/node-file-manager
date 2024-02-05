fa68de985e4ac2763047f14006f3eabc625b3982
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-02-05 23:57:06 +0300

    remove unused args

--- src/commands/filesystem/mv.js
+++ src/commands/filesystem/mv.js
@@ -1,8 +1,7 @@
-import { getCurrentDirectory } from '../../directory.js';
 import { cp } from './cp.js';
 import { rm } from './rm.js';
 
 export const mv = async (userInput) => {
-  await cp(userInput, getCurrentDirectory());
-  await rm(userInput, getCurrentDirectory());
+  await cp(userInput);
+  await rm(userInput);
 };
